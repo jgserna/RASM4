e33914da9506ab4560715f4f9d478881ff308291
jgserna <107096062+jgserna@users.noreply.github.com>
2023-11-05 21:16:49 -0800

    Update rasm4.s

--- rasm4.s
+++ rasm4.s
@@ -3,7 +3,7 @@
 //
 
 //
-	.equ	MAX_BYTES,21
+	.equ	MAX_BYTES, 512
 
 	.data
 szName:		.asciz	"Names:Austin Monroe & Jocelyne Gallardo"
@@ -12,9 +12,11 @@ szClass:	.asciz	"Class: CS3B"
 szDate:		.asciz	"Date: November 9, 2023"
 szMenu:		.asciz	"<1> View all strings\n\n<2> Add String\n\t<a> from Keyboard\n\t<b> from File\n\n<3> Delete String\n\n<4> Edit String\n\n<5> String Search\n\n<6> Save File\n\n<7> Quit\n\n" 
 szPrompt:	.asciz	"Enter your selection: "
+szPrompt1:	.asciz	"Enter a string: "
 szInvalidMsg:	.asciz	"Invalid entry."
-szGoodbye:	.asciz	"Goodbye."
+szGoodbye:	.asciz	"Goodbye.\n"
 szInput:	.skip	21
+strInput:	.skip	512
 chLF:		.byte	0xa
 
 
@@ -93,42 +95,53 @@ inputValid:
 	ldrb	w21,[x20]		//load first byte of x0(input)
 	
 //compare and branch to...
-	cmp	w21,#1
+	cmp	w21,#'1'
 	b.eq	viewAll
 	
-	cmp	w21,#2
-	b.eq	addFrom
+	cmp	w21,#'2'
+	b.eq	checkAorB
 	
-	cmp	w21,#3
+	cmp	w21,#'3'
 	b.eq	deleteStr
 	
-	cmp	w21,#4
+	cmp	w21,#'4'
 	b.eq	editStr
 	
-	cmp	w21,#5
+	cmp	w21,#'5'
 	b.eq	searchStr
 	
-	cmp	w21,#6
+	cmp	w21,#'6'
 	b.eq	saveFile
 	
-	cmp	w21,#7
+	cmp	w21,#'7'
 	b.eq	quit
 	
 // functions to make:
 viewAll:
-	//openfile
-	//read from File
-	//output all to screen
-	//close File
-	//b	inputLoop
-addFrom:
-	//load second byte of szInput
-	//perform check on second byte
-	//if 'a' perform add from KBD
-	//if 'b' perform add from File
+
+	b inputLoop
+	
+addStrFromKeyboard:
+	ldr	x0,=szPrompt1
+	bl	putstring
+	
+	ldr	x0,=strInput
+	mov	x1,#MAX_BYTES
+	bl	getstring
+	
+	ldr	x0,=strInput
+	bl	addFromKBD
+	
+	b inputLoop
+	
+addStringFromFile:
+
+	//ldr	x0,=fileName
+	//bl	addFromFile
+	
+	b inputLoop
 	
 	
-	//b inputLoop
 deleteStr:
 	//prompt user for index?
 	//use index in function to delete String
@@ -141,7 +154,17 @@ searchStr:
 
 saveFile:
 	
-		
+checkAorB:
+	ldr	x20,=szInput
+	add	x20,x20,#1
+	ldrb	w21,[x20]
+	
+	cmp	w21,#'a'
+	b.eq	addStrFromKeyboard
+	
+	cmp	w21,#'b'
+	b.eq	addStringFromFile
+	
 	
 quit:
 	ldr	x0,=szGoodbye
